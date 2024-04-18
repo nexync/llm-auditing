6c187d59e16979c9726e7816a46ae7c3e46f6964
Jeff <jeffrey.cheng132@gmail.com>
2024-04-18 14:53:48 -0400

    slrum

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+#SBATCH --partition=brtx6
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --partition=brtx6
 #SBATCH --time=2:00:0
 #SBATCH --job-name="adv attack"
 #SBATCH --output=%j.out
-#SBATCH --mem=16G
 
 echo date
 
