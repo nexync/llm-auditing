9f72fbeed208a97f7c1e84b0f4871221a320e7ab
Jeff <jeffrey.cheng132@gmail.com>
2024-04-18 14:52:53 -0400

    partition

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --partition=brtx2
+#SBATCH --partition=brtx6
 #SBATCH --time=2:00:0
 #SBATCH --job-name="adv attack"
 #SBATCH --output=%j.out
