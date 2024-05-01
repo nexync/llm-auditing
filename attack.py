241367e2e6523d648e42326da8eedcae8c6337a4
Jeff <jeffrey.cheng132@gmail.com>
2024-05-01 00:16:32 -0400

    shape

--- attack.py
+++ attack.py
@@ -333,6 +333,8 @@ class CausalDPAttack(BaseAdvAttack):
 
 			combined = torch.concat([surprisals.unsqueeze(1), suffix_batch], dim = 1)
 			combined = combined[torch.sort(combined[:, 0]).indices]
+			print(combined[:params["M"]//2].shape)
+			print(combined[(torch.randperm(combined.shape[0] - params["M"]//2, device=combined.device) + params["M"]//2)[:(params["M"]//2)]].shape)
 			beam = torch.stack([combined[:params["M"]//2], combined[(torch.randperm(combined.shape[0] - params["M"]//2, device=combined.device) + params["M"]//2)[:(params["M"]//2)]]], dim = 0)
 			print(beam.shape)
 
